d06e9bff37521fd5cf49bcd51e721036f57488b3
Theshy101 <t.s.w.f.us@gmail.com>
2021-05-01 13:05:31 +0300

    Added arrays.js

---
+++ 7-arrays.js
@@ -0,0 +1,23 @@
+// arrays/lists are a data structure that we use to represent a list of items
+
+let selectedColors = ['red','blue','green'];
+
+// we use [] to identify an array/list
+
+// each item inside the array has an index number starting from 0 EXAMPLE
+
+console.log(selectedColors[1]); // console shows blue
+
+// to access blue, instead of typing 2 we will type index 1 since index starts from 0
+
+// note that we use [] to access index numbers instead of (.)
+
+// ----------------------------------------------------------
+
+// since javascript is dynamic you can add different type of data types inside the same array/list EXAMPLE
+
+selectedColors[3] = 23;
+
+console.log(selectedColors[3]); // console shows 23
+
+console.typeof(selectedColors[2],selectedColors[3]);
