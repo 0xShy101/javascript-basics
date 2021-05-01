c56862e6f785c73bc8a7ef9ad6c34f3bd35f259d
Theshy101 <t.s.w.f.us@gmail.com>
2021-05-01 11:21:25 +0300

    adding 3-let-const.js

---
+++ 3-let-const.js
@@ -0,0 +1,9 @@
+// Const is something that is consistant and shouldn't be changed to avoid bugs in our applications 
+const name = 'theshy101';
+
+// let is something that might change in the future
+let age = 18;
+// changing age from 18 to 19
+age = 19;
+
+// there is also (var) but it's better using (let) instead due to some bugs in var
