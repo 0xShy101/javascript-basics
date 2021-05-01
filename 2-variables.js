a1ab96ba7be8b4ad38b964649c495283c76fae50
Theshy101 <t.s.w.f.us@gmail.com>
2021-05-01 10:57:18 +0300

    Adding Variables

---
+++ 2-variables.js
@@ -0,0 +1,24 @@
+let name = 'TheShy101';
+console.log(name);
+
+// Variable name rules:
+// Cannot be a reserved keyword
+// Should be meaningful
+// Cannot start with a number (1name)
+// Cannot contain a space or hyphen (-)
+
+// Variables are case-sensitive EXAMPLE
+
+let firstName = 'Shy';
+let FirstName = 'Alex';
+
+// these are completely different Variables
+
+// ----------------------------------------
+
+// You can have more than one Variable in a single line using (,) EXAMPLE
+
+let City = 'NYC', Country;
+
+// City is in this example is defined, We can know this by seeing that it has a (=)
+// Country is undefind because it does not contain a (=) in it
