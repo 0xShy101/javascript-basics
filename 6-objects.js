3a1a377eb7d10cada26622a435c183d4c5c58f52
Theshy101 <t.s.w.f.us@gmail.com>
2021-05-01 12:44:20 +0300

    Added objects.js

---
+++ 6-objects.js
@@ -0,0 +1,21 @@
+// an object is like an object in real life, think of a person, a person has must have a name and age for it to be a person EXAMPLE
+
+let person = {
+    name: 'shy',
+    age: 19,
+};
+
+/* 
+note that inside objects we end with (,) instead of (;)
+and use (:) instead of (=)
+*/
+
+console.log(person); // console shows { name: 'shy', age: 19 }
+
+// let's say we want to change the name inside the object, we will use Dot Notation
+
+person.name = 'theshy101';
+
+// this also works on console.log
+
+console.log(person.name); // console shows theshy101
