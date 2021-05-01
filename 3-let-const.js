12dc0333972efffc8d9257d63c69ac130307e2a5
Theshy101 <t.s.w.f.us@gmail.com>
2021-05-01 13:47:19 +0300

    Changed comment in let-const.js

--- 3-let-const.js
+++ 3-let-const.js
@@ -6,4 +6,4 @@ let age = 18;
 // changing age from 18 to 19
 age = 19;
 
-// there is also (var) but it's better using (let) instead due to some bugs in var
+// there is also (var) but it's better using (let) instead due to ES6 released made (let) and (const) the most common use while (var) remained simply to not break old websites
